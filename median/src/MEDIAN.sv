b96fff0c1a807bcc00be23ec5d949c5cf00c4a1e
Geoffroy MATEU <geoffroy.mateu@telecom-paristech.fr>
2019-12-09 12:05:36 +0100

    réécriture du bloc séquentiel sans les BYP qui vont être en combinatoire

--- median/src/MEDIAN.sv
+++ median/src/MEDIAN.sv
@@ -15,7 +15,7 @@ module MEDIAN #(parameter width = 8)
 
 
   MED #(.width(width))med0(.DI(DI_S),.DSI(DSI_S),.CLK(CLK),.DO(DO));
-  enum logic [3:0] {attente, chargement, S1, S2, S3, S4, S5} state;
+  enum logic [3:0] {ATTENTE, CHARGEMENT, S1, S2, S3, S4, S5} state;
 
 
   always @(posedge CLK or negedge nRST)begin
@@ -27,86 +27,69 @@ module MEDIAN #(parameter width = 8)
       DI_S<=DI;
       DSI_S<=DSI;
       case (state)
-        S0: begin
+
+        ATTENTE: if (DSI) begin
+          state <= CHARGEMENT;
+        end
+
+        CHARGEMENT: if (~DSI) begin
+          state <= S1;
+          cpt <= 0;
+        end
+
+        S1: begin
           if (cpt<8) begin
-            BYP <= 0;
             cpt <= cpt+1;
           end
           if (cpt==8) begin
-            BYP <=1 ;
             cpt <= 0;
-            state <= S1;
+            state <= S2;
           end
         end
-        S1: begin
-          if (cpt <7)begin
-            BYP <=0;
-            cpt <= cpt+1;
-          end
-          else begin
-            if (cpt<8) begin
-              BYP <= 1;
-              cpt <= cpt + 1;
-            end
-            else begin
-              BYP <= 1;
-              cpt <= 0;
-              state <= S2;
-            end
-          end
-        end
+
         S2: begin
-          if (cpt <6)begin
-            BYP <=0;
+          if (cpt <8)begin
             cpt <= cpt+1;
           end
-          else begin
-            if (cpt<8) begin
-              BYP <= 1;
-              cpt <= cpt + 1;
-            end
-            else begin
-              BYP <= 1;
-              cpt <= 0;
-              state <= S3;
-            end
+          if (cpt==8) begin
+            cpt <= 0;
+            state <= S3;
           end
         end
+
         S3: begin
-          if (cpt <5)begin
-            BYP <=0;
+          if (cpt <8)begin
             cpt <= cpt+1;
           end
-          else begin
-            if (cpt<8) begin
-              BYP <= 1;
-              cpt <= cpt + 1;
-            end
-            else begin
-              BYP <= 1;
-              cpt <= 0;
-              state <= S4;
-            end
+          if (cpt==8) begin
+            cpt <= 0;
+            state <= S4;
           end
         end
+
         S4: begin
-          if (cpt <5)begin
-            BYP <=0;
+          if (cpt <8)begin
             cpt <= cpt+1;
           end
+          if (cpt==8) begin
+            cpt <= 0;
+            state <= S5;
+          end
+        end
+
+        S5: begin
+          if (C == 4)begin
+            state <= ATTENTE;
+          end
           else begin
-            if (cpt<8) begin
-              BYP <= 1;
-              cpt <= cpt + 1;
-            end
-            else begin
-              BYP <= 1;
-              cpt <= 0;
-              DSO <=1;
-            end
+            cpt <= cpt + 1;
           end
         end
       endcase
     end
   end
+
+
+
+
 endmodule
