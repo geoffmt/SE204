846d59f48b8a1123eb6c5dbe8f121bc4b584cc79
Geoff <geoffhome@MacBook-Pro-de-Geoff.local>
2019-12-15 13:17:27 +0100

    fin de l'implémentation du registered feedback cycle ; test non effectué

--- wb_tp/controleur_memoire/wb_bram/wb_bram.sv
+++ wb_tp/controleur_memoire/wb_bram/wb_bram.sv
@@ -13,10 +13,13 @@ module wb_bram #(parameter mem_adr_width = 11) (
 logic [3:0][7:0] ram [0:2**mem_adr_width-1];
 //on ignore les deux bits de poids faible pour se déplacer de 32 bits en 32 bits
 wire [mem_adr_width-1:0] memory_slave = wb_s.adr[mem_adr_width+1:2];
+logic [mem_adr_width-1:0] memory_slave_cpt;//permet de décaler la mémoire pendant les bursts
 integer i;
 //l'acquitement de la lecture a un cycle de retard il faut donc créer
 //un signal ack_read
 logic ack_read;
+
+
 
 assign wb_s.ack = wb_s.stb && (wb_s.we || (!wb_s.we && ack_read));
 
@@ -27,9 +30,26 @@ always_ff@ (posedge wb_s.clk) begin
   if (wb_s.stb && !wb_s.we)begin
     ack_read<=1;
     // mode classique
-    if(!ack_read) wb_s.dat_sm <= ram[memory_slave];
-    else ack_read <= 0;
-
+    if (!wb_s.cti[0] && !wb_s.cti[1] && !wb_s.cti[2])begin 
+      if(!ack_read) wb_s.dat_sm <= ram[memory_slave];
+      else ack_read <= 0;
+    end
+    // mode constant address burst
+    else if (wb_s.cti[0] && !wb_s.cti[1] && !wb_s.cti[2])
+      wb_s.dat_sm <= ram[memory_slave];
+    // mode incremeting address burst
+    else begin 
+      // first step of the burst
+      if(!ack_read) begin 
+        wb_s.dat_sm <= ram[memory_slave];
+        memory_slave_cpt <= memory_slave + 1;
+      end
+      // let's continue the burst
+      else begin 
+        wb_s.dat_sm <= ram[memory_slave_cpt];
+        memory_slave_cpt <= memory_slave_cpt + 1;
+      end
+    end
   end
 end
 
@@ -37,7 +57,7 @@ end
 always_ff@(posedge wb_s.clk)
 begin
   if (wb_s.stb && wb_s.we)
-    for (i = 0;  i< 4; i++)
+    for (i = 0; i<4; i++)
       if(wb_s.sel[i])
         ram[memory_slave][i]<=wb_s.dat_ms[8*(i+1)-1 -: 8];
 end
